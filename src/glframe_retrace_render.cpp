ldeks/apitrace

refactor retracing into Render-specific delegate
When replacing shaders and changing state, each render will have a set
of overrides that apply to its retrace implementation.  This commit
splits calls into renders, and delegates retracing to those renders.

This refactor has the nice side effect of breaking up what was a
monolithic retrace implementation.

retrace/daemon/glframe_retrace_render.cpp
@@ -1,0 +1,103 @@
+/**************************************************************************
+ *
+ * Copyright 2015 Intel Corporation
+ * All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ **************************************************************************/
+
+#include "glframe_retrace_render.hpp"
+
+#include "glframe_state.hpp"
+#include "retrace.hpp"
+#include "trace_parser.hpp"
+
+using glretrace::RetraceRender;
+using glretrace::StateTrack;
+
+bool changesContext(const trace::Call * const call) {
+  if (strncmp(call->name(), "glXMakeCurrent", strlen("glXMakeCurrent")) == 0)
+    return true;
+  return false;
+}
+
+RetraceRender::RetraceRender(trace::Parser *parser,
+                             retrace::Retracer *retracer,
+                             StateTrack *tracker) : m_parser(parser),
+                                                    m_retracer(retracer) {
+  m_parser->getBookmark(m_bookmark.start);
+  trace::Call *call = NULL;
+  while ((call = parser->parse_call())) {
+    tracker->flush();
+    m_retracer->retrace(*call);
+    tracker->track(*call);
+    m_end_of_frame = call->flags & trace::CALL_FLAG_END_FRAME;
+    bool render = call->flags & trace::CALL_FLAG_RENDER;
+    delete call;
+
+    assert(!changesContext(call));
+    ++(m_bookmark.numberOfCalls);
+    if (render || m_end_of_frame)
+      break;
+  }
+}
+
+void
+RetraceRender::retraceRenderTarget() const {
+  // check that the parser is in correct state
+  trace::ParseBookmark bm;
+  m_parser->getBookmark(bm);
+  assert(bm.offset == m_bookmark.start.offset);
+
+  // play up to the end of the render
+  for (int calls = 0; calls < m_bookmark.numberOfCalls; ++calls) {
+    trace::Call *call = m_parser->parse_call();
+    assert(call);
+
+    // TODO(majanes): select the simple shader program if necessary
+
+    m_retracer->retrace(*call);
+    delete(call);
+  }
+}
+
+
+void
+RetraceRender::retrace(StateTrack *tracker) const {
+  // check that the parser is in correct state
+  trace::ParseBookmark bm;
+  m_parser->getBookmark(bm);
+  assert(bm.offset == m_bookmark.start.offset);
+
+  // play up to the end of the render
+  for (int calls = 0; calls < m_bookmark.numberOfCalls; ++calls) {
+    if (tracker)
+      tracker->flush();
+    trace::Call *call = m_parser->parse_call();
+    assert(call);
+
+    // TODO(majanes): select the simple shader program if necessary
+
+    m_retracer->retrace(*call);
+    if (tracker)
+      tracker->track(*call);
+    delete(call);
+  }
+}

retrace/daemon/glframe_retrace_render.hpp
@@ -1,0 +1,71 @@
+/**************************************************************************
+ *
+ * Copyright 2015 Intel Corporation
+ * All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ **************************************************************************/
+
+#ifndef _GLFRAME_RETRACE_RENDER_HPP_
+#define _GLFRAME_RETRACE_RENDER_HPP_
+
+#include <vector>
+
+#include "trace_loader.hpp"
+
+namespace trace {
+class Parser;
+}
+
+namespace retrace {
+class Retracer;
+}
+
+namespace glretrace {
+
+class StateTrack;
+class OnFrameRetrace;
+class ExperimentId;
+class MetricId;
+
+class RetraceRender {
+ public:
+  RetraceRender(trace::Parser *parser,
+                retrace::Retracer *retracer,
+                StateTrack *tracker);
+  void retraceRenderTarget() const;
+  void retrace(StateTrack *tracker = NULL) const;
+  bool endsFrame() const { return m_end_of_frame; }
+  void overrideProgram(int program);
+  void overrideRenderTargetProgram(int program);
+
+ private:
+  trace::Parser *m_parser;
+  retrace::Retracer *m_retracer;
+  trace::RenderBookmark m_bookmark;
+  int m_program;
+  int m_rt_program;
+  int m_override_program;
+  bool m_end_of_frame;
+};
+
+}  // namespace glretrace
+
+#endif  // _GLFRAME_RETRACE_RENDER_HPP_

joyxu/frameretrace

src/glframe_retrace_render.cpp
@@ -1,0 +1,103 @@
+/**************************************************************************
+ *
+ * Copyright 2015 Intel Corporation
+ * All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ **************************************************************************/
+
+#include "glframe_retrace_render.hpp"
+
+#include "glframe_state.hpp"
+#include "retrace.hpp"
+#include "trace_parser.hpp"
+
+using glretrace::RetraceRender;
+using glretrace::StateTrack;
+
+bool changesContext(const trace::Call * const call) {
+  if (strncmp(call->name(), "glXMakeCurrent", strlen("glXMakeCurrent")) == 0)
+    return true;
+  return false;
+}
+
+RetraceRender::RetraceRender(trace::AbstractParser *parser,
+                             retrace::Retracer *retracer,
+                             StateTrack *tracker) : m_parser(parser),
+                                                    m_retracer(retracer) {
+  m_parser->getBookmark(m_bookmark.start);
+  trace::Call *call = NULL;
+  while ((call = parser->parse_call())) {
+    tracker->flush();
+    m_retracer->retrace(*call);
+    tracker->track(*call);
+    m_end_of_frame = call->flags & trace::CALL_FLAG_END_FRAME;
+    bool render = call->flags & trace::CALL_FLAG_RENDER;
+    assert(!changesContext(call));
+    delete call;
+
+    ++(m_bookmark.numberOfCalls);
+    if (render || m_end_of_frame)
+      break;
+  }
+}
+
+void
+RetraceRender::retraceRenderTarget() const {
+  // check that the parser is in correct state
+  trace::ParseBookmark bm;
+  m_parser->getBookmark(bm);
+  assert(bm.offset == m_bookmark.start.offset);
+
+  // play up to the end of the render
+  for (int calls = 0; calls < m_bookmark.numberOfCalls; ++calls) {
+    trace::Call *call = m_parser->parse_call();
+    assert(call);
+
+    // TODO(majanes): select the simple shader program if necessary
+
+    m_retracer->retrace(*call);
+    delete(call);
+  }
+}
+
+
+void
+RetraceRender::retrace(StateTrack *tracker) const {
+  // check that the parser is in correct state
+  trace::ParseBookmark bm;
+  m_parser->getBookmark(bm);
+  assert(bm.offset == m_bookmark.start.offset);
+
+  // play up to the end of the render
+  for (int calls = 0; calls < m_bookmark.numberOfCalls; ++calls) {
+    if (tracker)
+      tracker->flush();
+    trace::Call *call = m_parser->parse_call();
+    assert(call);
+
+    // TODO(majanes): select the simple shader program if necessary
+
+    m_retracer->retrace(*call);
+    if (tracker)
+      tracker->track(*call);
+    delete(call);
+  }
+}

src/glframe_retrace_render.hpp
@@ -1,0 +1,71 @@
+/**************************************************************************
+ *
+ * Copyright 2015 Intel Corporation
+ * All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ **************************************************************************/
+
+#ifndef _GLFRAME_RETRACE_RENDER_HPP_
+#define _GLFRAME_RETRACE_RENDER_HPP_
+
+#include <vector>
+
+#include "glframe_retrace.hpp"
+
+namespace trace {
+class Parser;
+}
+
+namespace retrace {
+class Retracer;
+}
+
+namespace glretrace {
+
+class StateTrack;
+class OnFrameRetrace;
+class ExperimentId;
+class MetricId;
+
+class RetraceRender {
+ public:
+  RetraceRender(trace::AbstractParser *parser,
+                retrace::Retracer *retracer,
+                StateTrack *tracker);
+  void retraceRenderTarget() const;
+  void retrace(StateTrack *tracker = NULL) const;
+  bool endsFrame() const { return m_end_of_frame; }
+  void overrideProgram(int program);
+  void overrideRenderTargetProgram(int program);
+
+ private:
+  trace::AbstractParser *m_parser;
+  retrace::Retracer *m_retracer;
+  RenderBookmark m_bookmark;
+  int m_program;
+  int m_rt_program;
+  int m_override_program;
+  bool m_end_of_frame;
+};
+
+}  // namespace glretrace
+
+#endif  // _GLFRAME_RETRACE_RENDER_HPP_